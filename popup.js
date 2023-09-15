de4fee44abc48d474e33d47b301a4947cdb04858
Ron <imronras14@gmail.com>
2023-09-15 17:26:20 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    // Iterate through elements with data attributes
+    // iterate through elements with data attributes
     $('[data-popup^="open-"]').each(function () {
         const index = $(this).attr('data-popup').replace('open-', '');
-        // Open popup
+        // open popup
         $(this).click(function () {
             $(`[data-popup="item-${index}"]`).fadeIn();
         });
-        // Close popup
+        // close popup
         $(`[data-popup="close-${index}"]`).click(function () {
             $(`[data-popup="item-${index}"]`).fadeOut();
         });
