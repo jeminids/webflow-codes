d2243a17d79f9016f7eae691b66ac8bad36b8254
Ron <imronras14@gmail.com>
2023-09-15 17:23:29 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,14 +1,20 @@
-$(document).ready(function() {
-// Iterate through elements with data attributes
-$('[data-popup^="open-"]').each(function() {
-    const index = $(this).attr('data-popup').replace('open-', '');
-    // Open popup
-    $(this).click(function() {
-    $(`[data-popup="item-${index}"]`).fadeIn();
+$(document).ready(function () {
+    // Iterate through elements with data attributes
+    $('[data-popup^="open-"]').each(function () {
+        const index = $(this).attr('data-popup').replace('open-', '');
+        // Open popup
+        $(this).click(function () {
+            $(`[data-popup="item-${index}"]`).fadeIn();
+        });
+        // Close popup
+        $(`[data-popup="close-${index}"]`).click(function () {
+            $(`[data-popup="item-${index}"]`).fadeOut();
+        });
+        // set 'esc' to close
+        $(document).keydown(function (event) {
+            if (event.keyCode === 27) {
+                $(`[data-popup="item-${index}"]`).fadeOut();
+            }
+        });
     });
-    // Close popup
-    $(`[data-popup="close-${index}"]`).click(function() {
-    $(`[data-popup="item-${index}"]`).fadeOut();
-    });
-});
 });
