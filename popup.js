aed9ea1c0b6b4ad57d0b98ba625567d5505b5ff5
Ron <imronras14@gmail.com>
2023-09-16 10:41:22 +0700

    set body to overflow

--- popup.js
+++ popup.js
@@ -2,19 +2,26 @@ $(document).ready(function () {
     // iterate through elements with data attributes
     $('[data-popup^="open-"]').each(function () {
         const index = $(this).attr('data-popup').replace('open-', '');
+
         // open popup
         $(this).click(function () {
             $(`[data-popup="item-${index}"]`).fadeIn();
+            $("body").css("overflow", "hidden");
         });
+
         // close popup
         $(`[data-popup="close-${index}"]`).click(function () {
             $(`[data-popup="item-${index}"]`).fadeOut();
+            $("body").css("overflow", "");
         });
+
         // set 'esc' to close
         $(document).keydown(function (event) {
             if (event.keyCode === 27) {
                 $(`[data-popup="item-${index}"]`).fadeOut();
+                $("body").css("overflow", "");
             }
         });
+        
     });
 });
