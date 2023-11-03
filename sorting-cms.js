da5e28792eb1cdf7f5da3e18b69b03ae22d46c77
Ron <imronras14@gmail.com>
2023-11-03 09:23:23 +0700

    sorting-cms

---
+++ sorting-cms.js
@@ -0,0 +1,27 @@
+function sorting_prodcut() {
+var containers = $('[sort="true"]');
+
+containers.each(function () {
+    var container = $(this);
+    var items = container.find('[data-sort]');
+
+    items.sort(function (a, b) {
+    var aSort = parseInt($(a).attr('data-sort'));
+    var bSort = parseInt($(b).attr('data-sort'));
+
+    if (!isNaN(aSort) && !isNaN(bSort)) {
+        return aSort - bSort;
+    } else if (isNaN(aSort) && !isNaN(bSort)) {
+        return 1; 
+    } else if (!isNaN(aSort) && isNaN(bSort)) {
+        return -1; 
+    } else {
+        return 0;
+    }
+    });
+
+    container.empty().append(items);
+});
+}
+
+sorting_prodcut();
