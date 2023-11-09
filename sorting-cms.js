359729e3a06beda2054017e88fff711110c032a3
Ron <imronras14@gmail.com>
2023-11-09 10:02:29 +0700

    Update sorting-cms.js

--- sorting-cms.js
+++ sorting-cms.js
@@ -1,27 +1,23 @@
-function sorting_prodcut() {
-var containers = $('[data-sort="list"]');
+var cms_list = $('[role="list"]');
 
-containers.each(function () {
-    var container = $(this);
-    var items = container.find('[data-sort]');
+cms_list.each(function () {
+    var cms_item = $(this);
+    var items = cms_item.find('[data-sort]');
 
     items.sort(function (a, b) {
-    var aSort = parseInt($(a).attr('data-sort'));
-    var bSort = parseInt($(b).attr('data-sort'));
+        var aSort = parseInt($(a).attr('data-sort'));
+        var bSort = parseInt($(b).attr('data-sort'));
 
-    if (!isNaN(aSort) && !isNaN(bSort)) {
-        return aSort - bSort;
-    } else if (isNaN(aSort) && !isNaN(bSort)) {
-        return 1; 
-    } else if (!isNaN(aSort) && isNaN(bSort)) {
-        return -1; 
-    } else {
-        return 0;
-    }
+        if (!isNaN(aSort) && !isNaN(bSort)) {
+            return aSort - bSort;
+        } else if (isNaN(aSort) && !isNaN(bSort)) {
+            return 1;
+        } else if (!isNaN(aSort) && isNaN(bSort)) {
+            return -1;
+        } else {
+            return 0;
+        }
     });
 
-    container.empty().append(items);
+    cms_item.append(items);
 });
-}
-
-sorting_prodcut();
