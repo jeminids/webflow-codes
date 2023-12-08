96eedfea1d73205df4ef62a172d7ae9fadebe072
Ron <imronras14@gmail.com>
2023-12-08 22:30:29 +0700

    Create form-custom-selected.js

---
+++ form-custom-selected.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    // default state
+    $('.w-radio').removeClass('is-active');
+    $('.w-checkbox').removeClass('is-active');
+
+    // custom radio selected state
+    $('.w-radio').on('click', function () {
+        if ($(this).children().eq(1).is(':checked')) {
+            $(this).addClass('is-active');
+            $(this).siblings().removeClass('is-active');
+        }
+    });
+
+    // custom checkbox selected state
+    $('.w-checkbox').on('click', function () {
+        if ($(this).children().eq(0).is(':checked')) {
+            console.log("hey")
+            $(this).toggleClass('is-active');
+        }
+    });
+
+    // end
+});
