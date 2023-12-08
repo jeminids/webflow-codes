5e09ad3708f16213f8cdaa2636835a194fced620
Ron <imronras14@gmail.com>
2023-12-08 22:34:36 +0700

    Update form-custom-selected.js

--- form-custom-selected.js
+++ form-custom-selected.js
@@ -1,22 +1,21 @@
+// default state
+$('.w-radio').removeClass('is-active');
+$('.w-checkbox').removeClass('is-active');
+
 $(document).ready(function () {
-    // default state
-    $('.w-radio').removeClass('is-active');
-    $('.w-checkbox').removeClass('is-active');
+// custom radio selected state
+$('.w-radio').on('click', function () {
+if ($(this).children().eq(1).is(':checked')) {
+    $(this).addClass('is-active');
+    $(this).siblings().removeClass('is-active');
+}
+});
+// custom checkbox selected state
+$('.w-checkbox').on('click', function () {
+if ($(this).children().eq(0).is(':checked')) {
+    $(this).toggleClass('is-active');
+}
+});
 
-    // custom radio selected state
-    $('.w-radio').on('click', function () {
-        if ($(this).children().eq(1).is(':checked')) {
-            $(this).addClass('is-active');
-            $(this).siblings().removeClass('is-active');
-        }
-    });
-
-    // custom checkbox selected state
-    $('.w-checkbox').on('click', function () {
-        if ($(this).children().eq(0).is(':checked')) {
-            $(this).toggleClass('is-active');
-        }
-    });
-
-    // end
+// end
 });
