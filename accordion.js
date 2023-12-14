549e5fc01e7f08683ae181271563a09f4a989b17
Ron <imronras14@gmail.com>
2023-12-14 22:30:31 +0700

    Update accordion.js

--- accordion.js
+++ accordion.js
@@ -1,7 +1,23 @@
 $(document).ready(function () {
-    $("[accordion=detail]").slideUp()
+    function rotateIcon(icon, degrees) {
+        icon.animate({ borderSpacing: degrees }, {
+            step: function (now, fx) {
+                $(this).css('transform', 'rotate(' + now + 'deg)');
+            },
+            duration: 'fast'
+        }, 'linear');
+    }
+
+    $("[accordion=detail]").slideUp();
     $("[accordion=header]").click(function () {
-        $("[accordion=detail]").slideUp()
-        $(this).next("[accordion=detail]").slideDown()
+        var nextDetail = $(this).next("[accordion=detail]");
+        if (nextDetail.is(":visible")) {
+            return;
+        }
+        $("[accordion=detail]").slideUp();
+        nextDetail.slideDown();
+        var icon = $(this).children("[accordion=icon]");
+        rotateIcon($("[accordion=icon]"), 0);
+        rotateIcon(icon, 180);
     });
 });
