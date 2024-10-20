c7c00e487b12e9e5a419492c8edf9902732ac9cb
Ron <imronras14@gmail.com>
2024-10-20 22:44:34 +0700

    Create navbar-on-scroll.js

---
+++ navbar-on-scroll.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    function navbarScroll() {
+        var navbarHeight = $('[navbar]').outerHeight();
+        $('[navbar-height]').css('height', navbarHeight + 'px');
+        let lastScrollTop = 0;
+
+        $(window).on('scroll', function () {
+            const scrollTop = $(this).scrollTop();
+            if (scrollTop === 0) {
+                $('[navbar]').removeClass('is-hidden');
+            } else if (scrollTop > lastScrollTop) {
+                $('[navbar]').addClass('is-hidden');
+            } else {
+                $('[navbar]').removeClass('is-hidden');
+            }
+            lastScrollTop = scrollTop;
+        });
+    }
+
+    function runBasedOnNavbarValue() {
+        var navbarValue = $('[navbar]').attr('navbar'); 
+        var screenWidth = $(window).outerWidth();
+        console.log(screenWidth, navbarValue);
+
+        if (!navbarValue) {
+            navbarScroll(); 
+        } else if (navbarValue === "1" && screenWidth >= 991) {
+            navbarScroll(); 
+        } else if (navbarValue === "2" && screenWidth > 767) {
+            navbarScroll(); 
+        } else if (navbarValue === "3" && screenWidth <= 767) {
+            navbarScroll(); 
+        } else if (navbarValue.includes("1") && navbarValue.includes("3") && (screenWidth >= 991 || screenWidth <= 767)) {
+            navbarScroll(); 
+        } else if (navbarValue.includes("2") && navbarValue.includes("3") && screenWidth <= 991) {
+            navbarScroll(); 
+        }
+    }
+
+    runBasedOnNavbarValue();
+});
