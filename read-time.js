4290e428a8be2cf837787b0505529c5673da551c
Ron <imronras14@gmail.com>
2024-10-20 14:38:25 +0700

    Update read-time.js

--- read-time.js
+++ read-time.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    var content = $('[read-time="content"]').text();
+    var content = $('[read-time]').text();
     var wordCount = content.trim().split(/\s+/).length;
     var readingTime = Math.ceil(wordCount / 200);    
     var minuteText = 'minute';
     if (readingTime > 1) {
         minuteText = 'minutes';
     }    
-    $('[read-time="target"]').text(readingTime + ' ' + minuteText + ' read');
+    $('[read-time-count]').text(readingTime + ' ' + minuteText + ' read');
 });
