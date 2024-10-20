81dec00ff199f7aca0390b0cf8d313171c6502ca
Ron <imronras14@gmail.com>
2024-10-20 09:47:23 +0700

    Update accordion.js

--- accordion.js
+++ accordion.js
@@ -1,11 +1,15 @@
-$(document).ready(function () {
-    $('[accordion="item"]').each(function () {
-        var speed = $(this).attr('accordion-speed') || 300;     
-        $(this).find('[accordion="content"]').slideUp();    
-        $(this).find('[accordion="trigger"]').click(function() {
-            var $content = $(this).siblings('[accordion="content"]');
-            $('[accordion="content"]').not($content).slideUp(speed);
-            $content.stop(true, true).slideToggle(speed);
+$('[accordion-icon]').removeClass('is-open');
+
+$('[accordion-trigger]').each(function () {
+    var speed = parseInt($(this).attr('accordion-trigger')) || 300;
+    $(this).next('[accordion-content]').slideUp();
+
+    $(this).click(function () {
+        var $content = $(this).next('[accordion-content]');
+        var $icon = $(this).find('[accordion-icon]');
+        $('[accordion-content]').not($content).slideUp(speed).prev('[accordion-trigger]').find('[accordion-icon]').removeClass('is-open');
+        $content.stop(true, true).slideToggle(speed, function() {
+            $icon.toggleClass('is-open');
         });
-    });    
+    });
 });
