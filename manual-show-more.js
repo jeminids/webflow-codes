c200aa1497d7d120ee97f9d8496214aba28db935
Ron <imronras14@gmail.com>
2024-10-20 14:19:37 +0700

    Update manual-show-more.js

--- manual-show-more.js
+++ manual-show-more.js
@@ -1,17 +1,14 @@
 $(document).ready(function () {
-    var loadCount = $('[data-load="list"]').attr('load-count');
+    var loadCount = parseInt($('[show-more-list]').attr('show-more-list')) || 5;
 
-    // Initially hide all children of [data-load="list"] except for the loadCount
-    $('[data-load="list"]').children().hide();
-    $('[data-load="list"]').children().slice(0, loadCount).show();
+    $('[show-more-list]').children().hide();
+    $('[show-more-list]').children().slice(0, loadCount).show();
 
-    // When [data-load="button"] is clicked, show the next batch
-    $('[data-load="button"]').click(function () {
-        var hiddenChildren = $('[data-load="list"]').children(':hidden');
+    $('[show-more-button]').click(function () {
+        var hiddenChildren = $('[show-more-list]').children(':hidden');
         var childrenToShow = hiddenChildren.slice(0, loadCount);
         childrenToShow.show();
 
-        // If there are no more hidden children, hide the "load more" button
         if (hiddenChildren.length <= loadCount) {
             $(this).hide();
         }
