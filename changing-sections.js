ad4ea9bc5804a16f9f50bccfe6021d6f490cddb1
Ron <imronras14@gmail.com>
2024-10-21 12:23:13 +0700

    Create changing-sections.js

---
+++ changing-sections.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    var sectionIndex = localStorage.getItem('sectionIndex');
+    if (sectionIndex) {
+        sectionIndex++;
+    } else {
+        sectionIndex = 1;
+    }
+    var totalHeroes = $('[changing]').length;
+    if (sectionIndex > totalHeroes) {
+        sectionIndex = 1;
+    }
+    $('[changing]').hide();
+    $('[changing="' + sectionIndex + '"]').show();
+    localStorage.setItem('sectionIndex', sectionIndex);
+});
