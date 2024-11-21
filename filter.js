16e070d8b4107fa45df174d64f88b46c43042dba
Ron <imronras14@gmail.com>
2024-11-21 12:31:02 +0700

    Create filter.js

---
+++ filter.js
@@ -0,0 +1,44 @@
+// not-published v1.0
+
+var Webflow = Webflow || [];
+Webflow.push(function () {
+    $('[filter_no-result]').hide();
+
+    // Add 'All' button
+    let buttonAllText = $('[filter-button-all]').attr('filter-button-all') || 'All';
+    let buttonClone = $('[filter-button]').first().closest('[role="listitem"]').clone();
+    buttonClone.find('*').text(buttonAllText)
+        .end()
+        .find('[filter-button]')
+        .addClass('is-active')
+        .attr('filter-button', 'All')
+        .prependTo($('[filter-button]').first().closest('[role="list"]'));
+
+    // Filter logic
+    $('[filter-button]').click(function () {
+        const selectedText = $(this).attr('filter-button');
+        let isMatchFound = false;
+
+        // Toggle active state
+        $('[filter-button]').removeClass('is-active');
+        $(this).addClass('is-active');
+
+        const filterItems = $('[filter-text-list]');
+        const noResult = $('[filter_no-result]');
+
+        if (selectedText === 'All') {
+            filterItems.closest('[role="listitem"]').show();
+            noResult.hide();
+        } else {
+            filterItems.each(function () {
+                const itemText = $(this).text();
+                const listItem = $(this).closest('[role="listitem"]');
+                const isVisible = itemText.includes(selectedText);
+                listItem.toggle(isVisible);
+                isMatchFound = isMatchFound || isVisible;
+            });
+
+            noResult.toggle(!isMatchFound);
+        }
+    });
+});
